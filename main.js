fe60dbf603efd0d2d64a87ad6ebca15cb76ade75
Justin Zhao <89896879+justinzhao07@users.noreply.github.com>
2023-04-08 22:34:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 const api = {
   key: "3ab0797a52ffa359b7cf3af8b949e5fd",
-  base: "https://api.openweathermap.org/data/2.5/"
 }
 
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
-
-function setQuery(evt) {
   if (evt.keyCode == 13) {
     getResults(searchbox.value);
   }
 }
 
 function getResults (query) {
-  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+  fetch(`$https://api.openweathermap.org/data/2.5/weather?q=${query}&APPID=${api.key}`)
     .then(weather => {
       return weather.json();
     }).then(displayResults);
